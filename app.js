33bb191c3acd7c249759ab3d4b0f957f33f3c998
Manoj Soni <manojncsoni@gmail.com>
2021-04-17 15:51:22 +0530

    final psuh

---
+++ app.js
@@ -0,0 +1,28 @@
+console.log('connectd');
+
+const inputForm = document.querySelector('form');
+inputForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const inputWeight = document.querySelector('#weight');
+    const inputTanch = document.querySelector('#tanch');
+    const inputWantTanch = document.querySelector('#wTanch');
+
+    let weight = inputWeight.value;
+    let tanch = inputTanch.value;
+    let wTanch = inputWantTanch.value;
+    const resultsDOM = document.querySelector('.results');
+    const pure = document.querySelector('#pure');
+
+    let fine = (weight * tanch) / 100;
+    let other = weight - fine;
+    let divs = 100 - wTanch;
+    let total = other / divs;
+    let sub = total * 100;
+    let add = sub - weight;
+
+    resultsDOM.classList.add('showItem');
+    pure.textContent = `${add}`;
+
+
+})
